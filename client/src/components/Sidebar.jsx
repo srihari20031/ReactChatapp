b56cd6c79cd883d6403868c59c458bb9ac288938
srihari20031 <sriharivelayutham.28@gmail.com>
2024-02-01 19:10:17 +0530

    Second Commit

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,8 +4,11 @@ import Chats from "./Chats";
 import { signOut } from "firebase/auth";
 import { auth } from "../config/Firebase.config";
 import Search from "./Search";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Sidebar = () => {
+  const { currentuser } = useContext(AuthContext);
   return (
     <div className="flex-1 relative shadow-[rgba(255,255,255,0.1)_0px_1px_1px_0px_inset,rgba(50,50,93,0.25)_0px_50px_100px_-20px,rgba(0,0,0,0.3)_0px_30px_60px_-30px]  bg-[#314158] ">
       <div className="flex p-4 justify-between items-center  bg-[rgb(31,41,55)]  h-[75px]">
@@ -18,7 +21,7 @@ const Sidebar = () => {
         <div>
           <div className="flex gap-4">
             <img
-              src={Profile}
+              src={currentuser.photoURL}
               alt=""
               className="w-[45px] h-[45px] rounded-[50%]"
             />
